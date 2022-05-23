50c967d615ab3a8dc119f048b10ad75ccf6ac40d
Kyle Onda <konda@lincolninst.edu>
2022-05-23 12:24:25 -0400

    Delete R directory

--- R/01_ingest.R
+++
@@ -1,9 +0,0 @@
-
-# list all contributions
-l <- list.files("contributions",recursive=TRUE, pattern='*.geojson')
-
-# read them all
-d <- lapply(l, sf::st_read)
-
-# combine them all
-d <- do.call(rbind,d)
